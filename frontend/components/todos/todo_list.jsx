cd353c599503f7ed72f229e33c0a90278e39a185
Dan Luo <pichumy@gmail.com>
2018-05-28 16:20:26 -0700

    finish phase 4

--- frontend/components/todos/todo_list.jsx
+++ frontend/components/todos/todo_list.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import TodoListItem from './todo_list_item';
+import TodoForm from './todo_form';
 
 const TodoListContainer = (props) => {
-  console.log(props);
   const listItems = props.todos.map((todo, idx) => (
       <TodoListItem key={idx} todo={todo} />
   ));
 
   return (
-    <ul>
-      {listItems}
-    </ul>
+    <div>
+      <ul>
+        {listItems}
+      </ul>
+      <TodoForm receiveTodo={props.receiveTodo} />
+    </div>
   );
 };
 
